08bdf46a2be8b8354a2b40fc1293a2c657fcff8b
Pratik Parmar <steveparmar6nov2011@gmail.com>
2019-05-03 13:36:06 +0530

    Add course using APIView

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -5,8 +6,24 @@ from . import models
 from . import serializers
 
 
-class ListCourse(APIView):
-    def get(self, request, format=None):  # responses only to the get request
+class ListCreateCourse(APIView):
+    def get(self, request, format=None):
+        """
+        Returns a list of courses
+        Responses only to the get request
+        """
         courses = models.Course.objects.all()
         serializer = serializers.CourseSerializer(courses, many=True)
         return Response(serializer.data)
+
+    def post(self, request, format=None):
+        """
+        Takes data from post request, check if it's valid and save it to database.
+        Works almost as same as model form
+        Responses only to the post request
+        """
+        serializer = serializers.CourseSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
